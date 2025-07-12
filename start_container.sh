fc88264e37d1471002557160bb9f3c90914a07e6
SyedShakeel53 <65948841+SyedShakeel53@users.noreply.github.com>
2025-07-12 01:28:07 -0500

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull syedshakeel5/simple-python-project
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 syedshakeel5/simple-python-project
